5d021eb9e733a17b9bf45876fd3c8b157f88a9d4
SALMAN BUHARI FARIS <buharifarissalman@gmail.com>
2026-09-09 16:05:01 +0800

    Update script.js Excercise

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const totalAssets = 500;
 const openTickets = 24;
 const users = 42;
 const locations = 18;
+const systems = 75;
 
 console.log("ITOps Portal loaded");
 
@@ -9,3 +10,4 @@ document.getElementById("total-assets").textContent = totalAssets;
 document.getElementById("open-tickets").textContent = openTickets;
 document.getElementById("users").textContent = users;
 document.getElementById("locations").textContent = locations;
+document.getElementById("systems").textContent = systems;
